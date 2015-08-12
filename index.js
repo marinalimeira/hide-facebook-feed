6ff924d8a98280d87635c5969f18d7dbeb601069
@ivomarsan <contato@ivomarsantos.com>
2015-08-12 16:17:53 -0300

    :+1: Update contentScript

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
   include: "www.facebook.com",
-  contentScript: 'document.getElementById("topnews_main_stream_408239535924329").style.display="none";'
+  contentScript: 'document.getElementById("stream_pagelet").removeChild(document.getElementById("topnews_main_stream_408239535924329"));'
 });

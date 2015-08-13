863865898746de99306a3c385d48628742ce58eb
Caio Tiago Oliveira <asrail@gmail.com>
2015-08-13 00:46:43 -0300

    Using a wildcard for facebook domains.
    It will work with m.facebook, for instance.
    Also, it wouldn't wok properly anyway.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
-  include: "www.facebook.com",
+  include: "*.facebook.com",
   contentScript: 'document.getElementById("stream_pagelet").removeChild(document.getElementById("topnews_main_stream_408239535924329"));'
 });

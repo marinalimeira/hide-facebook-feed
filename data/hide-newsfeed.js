7a66f51e60a3ceaeae5bd14d2184a9f634839e61
Caio Tiago Oliveira <asrail@gmail.com>
2015-08-13 01:23:24 -0300

    Using a better approach for finding the newsfeed.
    Plus a better structuring. A separate file is recommended for non
    trivial extensions.

---
+++ data/hide-newsfeed.js
@@ -0,0 +1,9 @@
+var stream = document.getElementById("stream_pagelet");
+var topnews_regex = new RegExp(/^topnews_main_stream/);
+
+if (stream) {
+  for (let el of stream.childNodes) {
+    if (topnews_regex.test(el.id))
+      stream.removeChild(el);
+  }
+}
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
   include: "*.facebook.com",
-  contentScript: 'document.getElementById("stream_pagelet").removeChild(document.getElementById("topnews_main_stream_408239535924329"));'
+  contentScriptFile: "./hide-newsfeed.js"
 });

d0855fa131ae19a101952305452fe34413dcd582
Marina Limeira <marinaflessa@gmail.com>
2015-09-07 14:43:34 -0300

    Add onLoad event

--- data/hide-newsfeed.js
+++ data/hide-newsfeed.js
@@ -1,9 +1,10 @@
-var stream = document.getElementById("stream_pagelet");
-var topnews_regex = new RegExp(/^topnews_main_stream/);
-
-if (stream) {
-  for (let el of stream.childNodes) {
-    if (topnews_regex.test(el.id))
-      stream.removeChild(el);
+window.addEventListener("load", function() {
+  var stream = document.getElementById("stream_pagelet");
+  var topnews_regex = new RegExp(/^topnews_main_stream/);
+  if (stream) {
+    for (let el of stream.childNodes) {
+      if (topnews_regex.test(el.id))
+        stream.removeChild(el);
+    }
   }
-}
+});
